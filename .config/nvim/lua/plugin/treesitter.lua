0e3fe9f22d09deaaadaa47ecc9017a785264fe46
Eren <eren.simsek@tuta.io>
2023-09-22 09:00:01 +0100

    autosave

--- .config/nvim/lua/plugin/treesitter.lua
+++ .config/nvim/lua/plugin/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup {
-    ensure_installed = { 'lua', 'python', 'rust', 'tsx', 'typescript', 'bash', 'nu' },
+    ensure_installed = { 'lua', 'python', 'rust', 'tsx', 'typescript', 'bash' },
 
     auto_install = true,
 
