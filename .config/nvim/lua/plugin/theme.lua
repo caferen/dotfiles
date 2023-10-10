b18263c5c5cac1db9912b28d7ee599b82576b09f
Eren <eren.simsek@tuta.io>
2023-10-10 17:00:01 +0100

    autosave

--- .config/nvim/lua/plugin/theme.lua
+++ .config/nvim/lua/plugin/theme.lua
@@ -1,4 +1,4 @@
 vim.cmd('colorscheme base16-windows-95')
-vim.api.nvim_set_hl(0, 'Normal', { guibd = nil })
-vim.api.nvim_set_hl(0, 'SignColumn', { guibd = nil })
-vim.api.nvim_set_hl(0, 'LineNr', { guibd = nil })
+-- vim.api.nvim_set_hl(0, 'Normal', { guibd = nil })
+-- vim.api.nvim_set_hl(0, 'SignColumn', { guibd = nil })
+-- vim.api.nvim_set_hl(0, 'LineNr', { guibd = nil })
