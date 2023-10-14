9ccf97060552bec39c44c416588a7d100c750d80
Eren <eren.simsek@tuta.io>
2023-10-14 18:00:02 +0100

    autosave

--- .config/nvim/lua/plugin/git.lua
+++ .config/nvim/lua/plugin/git.lua
@@ -5,8 +5,5 @@ require('gitsigns').setup({
     current_line_blame_opts = {
         delay = 100,
     },
+    map("n", "<leader>c", package.loaded.gitsigns.blame_line, { desc = "Show git [c]ommit message for the current line" })
 })
-
-map("n", "<leader>c", function()
-    package.loaded.gitsigns.blame_line { full = true }
-end, { desc = "Show git [c]ommit message for the current line" })
