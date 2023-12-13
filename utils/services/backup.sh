14559c2dfc7c8c2fa1bfbec58d8f4d7258b38d79
Eren <eren.simsek@tuta.io>
2023-12-13 00:56:15 +0000

    autosave

---
+++ utils/services/backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mount /home/eren/backup
+/usr/bin/rsync -a --delete /home/eren/ssd/drive-cipher /home/eren/backup
+/usr/bin/rsync -a --delete /home/eren/ssd/media-cipher /home/eren/backup
+umount /home/eren/backup
