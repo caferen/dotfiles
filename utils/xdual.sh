7a3852fa825844803e991590eafc199d903ee9e3
Eren <eren.simsek@tuta.io>
2023-12-18 13:43:58 +0000

    autosave

--- utils/xdual.sh
+++ utils/xdual.sh
@@ -5,8 +5,8 @@ if [[ "$(echo $XDG_SESSION_TYPE)" == 'x11' ]]; then
     nvidia-settings --load-config-only &
 
     xrandr --output DP-4 --mode 2560x1440 --rotate normal --rate 165 --primary \
-        --output HDMI-0 --mode 1920x1080 --right-of DP-4 --rate 75 --rotate left
+        --output HDMI-0 --mode 1920x1080 --left-of DP-4 --rate 75 --rotate right
 else
     kscreen-doctor output.DP-4.enable output.DP-4.mode.3 output.DP-4.position.0,0 \
-        output.HDMI-A-3.enable output.HDMI-A-3.mode.1 output.HDMI-A-3.position.2560,0
+        output.HDMI-A-3.enable output.HDMI-A-3.mode.1 output.HDMI-A-3.position.-1080,0
 fi
