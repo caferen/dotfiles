6aa0fdf0ba2edc4ef0ca3195798eba47e50b3d10
Eren <eren.simsek@tuta.io>
2024-03-21 15:00:09 +0000

    autosave

--- .config/firefox/prefsCleaner.sh
+++ .config/firefox/prefsCleaner.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 
 ## prefs.js cleaner for Linux/Mac
 ## author: @claustromaniac
--- .config/firefox/updater.sh
+++ .config/firefox/updater.sh
@@ -1,5 +1,4 @@
-#!/bin/sh
-
+#!/usr/bin/env bash
 
 ## arkenfox user.js updater for macOS and Linux
 
